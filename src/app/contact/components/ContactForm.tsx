0b028e17d314fb8aac9235bd29510a60268468c5
FransAr16 <fransaryadita@gmail.com>
2025-02-23 03:14:49 +0700

    update14

--- src/app/contact/components/ContactForm.tsx
+++ src/app/contact/components/ContactForm.tsx
@@ -2,6 +2,7 @@
 
 import React, { useRef, useState } from "react";
 import { motion, useInView } from "motion/react";
+import emailjs from "@emailjs/browser";
 import TextFadeIn from "@/components/ui/TextFadeIn";
 import { fadeIn } from "@/data/animation";
 import ChevronDown from "@/components/ui/ChevronDown";
@@ -21,7 +22,7 @@ export default function ContactForm() {
   const colTwo = useRef<HTMLDivElement | null>(null);
   const colThree = useRef<HTMLDivElement | null>(null);
   const colFour = useRef<HTMLDivElement | null>(null);
-  const BtnSubmit = useRef<HTMLDivElement | null>(null);
+  const BtnSubmit = useRef<HTMLButtonElement | null>(null);
 
   const isInViewOne = useInView(colOne, { once: true });
   const isInViewTwo = useInView(colTwo, { once: true });
@@ -30,16 +31,89 @@ export default function ContactForm() {
   const isInViewBtnSubmit = useInView(BtnSubmit, { once: true });
   const [isOpen, setIsOpen] = useState(false);
   const [selectedReason, setSelectedReason] = useState("Select reason");
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    nameOrganization: "",
+    message: "",
+  });
+  const [errors, setErrors] = useState({
+    name: "",
+    email: "",
+    nameOrganization: "",
+    message: "",
+    reason: "",
+  });
 
   const toggleModal = () => setIsOpen(!isOpen);
   const selectReason = (reason: string) => {
     setSelectedReason(reason);
     setIsOpen(false);
+    setErrors((prev) => ({ ...prev, reason: "" })); // Hapus error jika sudah dipilih
   };
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+    setErrors({ ...errors, [e.target.name]: "" }); // Hapus error jika diisi
+  };
+
+  const validateForm = () => {
+    let newErrors = {
+      name: "",
+      email: "",
+      nameOrganization: "",
+      message: "",
+      reason: "",
+    };
+    if (!formData.name.trim()) newErrors.name = "Name is required.";
+    if (!formData.email.trim()) newErrors.email = "Email is required.";
+    if (!formData.nameOrganization.trim())
+      newErrors.nameOrganization = "Name organization is required.";
+    if (!formData.message.trim()) newErrors.message = "Message is required.";
+    if (!selectedReason) newErrors.reason = "Reason of contact is required.";
+    // if (selectedReason === "Select reason") newErrors.reason = "Reason of contact is required.";
+
+    setErrors(newErrors);
+    return Object.values(newErrors).every((error) => error === ""); // Return true jika tidak ada error
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!validateForm()) return;
+
+    const templateParams = {
+      reason: selectedReason,
+      name: formData.name,
+      email: formData.email,
+      nameOrganization: formData.nameOrganization,
+      message: formData.message,
+    };
+
+    try {
+      await emailjs.send(
+        "service_nq8r71e",
+        "template_v7e46np",
+        templateParams,
+        "16ExcdezLf3Oe9pGL"
+      );
+      alert("Your message has been sent successfully!");
+      setFormData({ name: "", email: "", nameOrganization: "", message: "" });
+      setSelectedReason(selectedReason);
+    } catch (error) {
+      console.error("Error sending email:", error);
+      alert("Failed to send message. Please try again.");
+    }
+  };
+
 
   return (
     <div className="flex flex-col w-full h-full pt-[2rem] pb-[4rem] lg:py-[2rem] 2xl:py-[4rem] relative overflow-hidden">
-      <div className="grid grid-cols-4 lg:grid-cols-12 w-full h-full main-container pt-[3rem] lg:pt-[2.5rem] border-t gap-[2rem]">
+      <form
+        onSubmit={handleSubmit}
+        className="grid grid-cols-4 lg:grid-cols-12 w-full h-full main-container pt-[3rem] lg:pt-[2.5rem] border-t gap-[2rem]"
+      >
         <div className="pb-[4rem] sm:pb-[3rem] lg:pb-0 col-span-4 lg:col-span-3">
           <TextFadeIn
             slideUp={fadeIn}
@@ -50,7 +124,7 @@ export default function ContactForm() {
         <div className="hidden lg:block lg:col-span-1">{/* blank */}</div>
         <div className="col-span-4 lg:col-span-8">
           <div className="flex flex-col">
-            {/* col 1 */}
+            {/* Reason of Contact */}
             <div className="flex flex-col lg:flex-row items-center gap-[1rem] lg:gap-[2rem] pb-[3rem] lg:pb-[1rem]">
               <div className="w-full lg:w-[16rem] 2xl:w-[24rem] 3xl:w-[33rem]">
                 <TextFadeIn
@@ -71,7 +145,7 @@ export default function ContactForm() {
                   <div className="flex items-center justify-between">
                     <div className="w-full overflow-hidden">
                       <span className="font-medium lg:font-semibold text-[1.1rem] sm:text-[1.2rem] lg:text-[1.1rem] 2xl:text-[1.28rem] leading-[120%]">
-                        {selectedReason}
+                        {selectedReason || "Select reason"}
                       </span>
                     </div>
                     <motion.div
@@ -93,7 +167,7 @@ export default function ContactForm() {
                       : "polygon(0 0, 100% 0, 100% 0, 0 0)",
                   }}
                   transition={{ duration: 0.5, ease: "easeInOut" }}
-                  className={`absolute -bottom-[196%] w-full flex flex-col bg-foreground text-background ${
+                  className={`absolute -bottom-[196%] w-full flex flex-col bg-foreground text-background z-[999] ${
                     isOpen ? "flex" : ""
                   }`}
                 >
@@ -113,11 +187,16 @@ export default function ContactForm() {
                       Contact for a job
                     </span>
                   </div>
+                  {errors.reason && (
+                    <p className="absolute bottom-0 px-6 lg:px-5 xl:px-6 2xl:px-7 text-red-500 text-sm mt-1">
+                      {errors.reason}
+                    </p>
+                  )}
                 </motion.div>
               </div>
             </div>
 
-            {/* col 2 */}
+            {/* Full Name */}
             <div className="flex flex-col lg:flex-row items-center gap-[1rem] lg:gap-[2rem] pb-[3rem] lg:pb-[1rem]">
               <div className="w-full lg:w-[16rem] 2xl:w-[24rem] 3xl:w-[33rem]">
                 <TextFadeIn
@@ -127,7 +206,7 @@ export default function ContactForm() {
                 />
               </div>
 
-              <div ref={colTwo} className="w-full">
+              <div ref={colTwo} className="w-full relative">
                 <motion.div
                   initial="initial"
                   animate={isInViewTwo ? "animate" : "initial"}
@@ -136,14 +215,22 @@ export default function ContactForm() {
                 >
                   <input
                     type="text"
+                    name="name"
+                    value={formData.name}
+                    onChange={handleChange}
                     className="w-full font-medium lg:font-semibold text-[1.1rem] sm:text-[1.2rem] lg:text-[1.1rem] 2xl:text-[1.28rem] leading-[120%] ring-0 outline-0 p-6 lg:p-5 xl:p-6 2xl:p-7"
                     placeholder="Full name here*"
                   />
+                  {errors.name && (
+                    <p className="absolute bottom-0 px-6 lg:px-5 xl:px-6 2xl:px-7 text-red-500 text-sm mt-1">
+                      {errors.name}
+                    </p>
+                  )}
                 </motion.div>
               </div>
             </div>
 
-            {/* col 3 */}
+            {/* Email Address */}
             <div className="flex flex-col lg:flex-row items-center gap-[1rem] lg:gap-[2rem] pb-[3rem] lg:pb-[1rem]">
               <div className="w-full lg:w-[16rem] 2xl:w-[24rem] 3xl:w-[33rem]">
                 <TextFadeIn
@@ -153,7 +240,7 @@ export default function ContactForm() {
                 />
               </div>
 
-              <div ref={colThree} className="w-full">
+              <div ref={colThree} className="w-full relative">
                 <motion.div
                   initial="initial"
                   animate={isInViewThree ? "animate" : "initial"}
@@ -161,15 +248,23 @@ export default function ContactForm() {
                   className="border"
                 >
                   <input
-                    type="text"
+                    type="email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
                     className="w-full font-medium lg:font-semibold text-[1.1rem] sm:text-[1.2rem] lg:text-[1.1rem] 2xl:text-[1.28rem] leading-[120%] ring-0 outline-0 p-6 lg:p-5 xl:p-6 2xl:p-7"
                     placeholder="example@provider.com*"
                   />
+                  {errors.email && (
+                    <p className="absolute bottom-0 px-6 lg:px-5 xl:px-6 2xl:px-7 text-red-500 text-sm mt-1">
+                      {errors.email}
+                    </p>
+                  )}
                 </motion.div>
               </div>
             </div>
 
-            {/* col 4 */}
+            {/* Name Organization */}
             <div className="flex flex-col lg:flex-row items-center gap-[1rem] lg:gap-[2rem] pb-[3rem] lg:pb-[1rem]">
               <div className="w-full lg:w-[16rem] 2xl:w-[24rem] 3xl:w-[33rem]">
                 <TextFadeIn
@@ -179,7 +274,7 @@ export default function ContactForm() {
                 />
               </div>
 
-              <div ref={colThree} className="w-full">
+              <div ref={colThree} className="w-full relative">
                 <motion.div
                   initial="initial"
                   animate={isInViewThree ? "animate" : "initial"}
@@ -188,14 +283,22 @@ export default function ContactForm() {
                 >
                   <input
                     type="text"
+                    name="nameOrganization"
+                    value={formData.nameOrganization}
+                    onChange={handleChange}
                     className="w-full font-medium lg:font-semibold text-[1.1rem] sm:text-[1.2rem] lg:text-[1.1rem] 2xl:text-[1.28rem] leading-[120%] ring-0 outline-0 p-6 lg:p-5 xl:p-6 2xl:p-7"
                     placeholder="John & Doe&reg;"
                   />
+                  {errors.nameOrganization && (
+                    <p className="absolute bottom-0 px-6 lg:px-5 xl:px-6 2xl:px-7 text-red-500 text-sm mt-1">
+                      {errors.nameOrganization}
+                    </p>
+                  )}
                 </motion.div>
               </div>
             </div>
 
-            {/* col 5 */}
+            {/* Message */}
             <div className="flex flex-col lg:flex-row items-center lg:items-start gap-[1rem] lg:gap-[2rem] lg:pb-[1rem]">
               <div className="w-full lg:w-[16rem] 2xl:w-[24rem] 3xl:w-[33rem] lg:pt-[1.3rem] xl:pt-[1.6rem] 2xl:pt-[1.8rem]">
                 <TextFadeIn
@@ -205,7 +308,7 @@ export default function ContactForm() {
                 />
               </div>
 
-              <div ref={colFour} className="w-full">
+              <div ref={colFour} className="w-full relative">
                 <motion.div
                   initial="initial"
                   animate={isInViewFour ? "animate" : "initial"}
@@ -213,9 +316,17 @@ export default function ContactForm() {
                   className="border"
                 >
                   <textarea
+                    name="message"
+                    value={formData.message}
+                    onChange={handleChange}
                     className="w-full h-[10rem] 2xl:h-[14rem] resize-none font-medium lg:font-semibold text-[1.1rem] sm:text-[1.2rem] lg:text-[1.1rem] 2xl:text-[1.28rem] leading-[120%] ring-0 outline-0 p-6 lg:p-5 xl:p-6 2xl:p-7"
                     placeholder="Start typing here ..."
-                  ></textarea>
+                  />
+                  {errors.message && (
+                    <p className="absolute bottom-0 px-6 lg:px-5 xl:px-6 2xl:px-7 text-red-500 text-sm mt-1">
+                      {errors.message}
+                    </p>
+                  )}
                 </motion.div>
               </div>
             </div>
@@ -224,7 +335,7 @@ export default function ContactForm() {
             <div className="flex flex-col lg:flex-row items-center gap-[1rem] lg:gap-[2rem]">
               <div className="w-full lg:w-[16rem] 2xl:w-[24rem] 3xl:w-[33rem]"></div>
 
-              <div ref={BtnSubmit} className="w-full relative">
+              <button type="submit" ref={BtnSubmit} className="w-full relative">
                 <motion.div
                   initial="initial"
                   animate={isInViewBtnSubmit ? "animate" : "initial"}
@@ -232,7 +343,7 @@ export default function ContactForm() {
                   className="border p-6 lg:p-5 xl:p-6 2xl:p-7 cursor-pointer bg-foreground text-background"
                 >
                   <div className="flex items-center justify-between">
-                    <div className="w-full overflow-hidden">
+                    <div className="w-full overflow-hidden flex ">
                       <span className="font-medium lg:font-semibold text-[1.1rem] sm:text-[1.2rem] lg:text-[1.1rem] 2xl:text-[1.28rem] leading-[120%]">
                         Submit form
                       </span>
@@ -242,11 +353,11 @@ export default function ContactForm() {
                     </div>
                   </div>
                 </motion.div>
-              </div>
+              </button>
             </div>
           </div>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
